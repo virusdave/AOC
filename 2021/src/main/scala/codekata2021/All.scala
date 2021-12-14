8a6159972d66c04353350e6d6422e0197dcf537b
Dave Nicponski <dave.nicponski@gmail.com>
2021-12-14 01:42:37 -0500

    2021 Day 14 (disgusting version)

--- 2021/src/main/scala/codekata2021/All.scala
+++ 2021/src/main/scala/codekata2021/All.scala
@@ -33,6 +33,7 @@ object All extends App {
 
   private def solutions: Seq[Puzzle] = Seq(
     days.Latest,
+    days.Day14,
     days.Day13,
     days.Day12,
     days.Day11,
---
+++ 2021/src/main/scala/codekata2021/days/Day14.scala
@@ -0,0 +1,86 @@
+package codekata2021
+package days
+
+import zio.RIO
+
+object Day14 extends ParserPuzzle {
+  override type PuzzleOut = Any
+  override def dayNum: Int = 14
+
+  val num: Parser[Int]                               = "[0-9]+".r ^^ (_.toInt)
+  val char: Parser[String]                           = "[A-Z]".r
+  val replacements: Parser[(String, String, String)] = char ~ char ~ ("->" ~> char) ^^ { case l ~ m ~ r => (l, m, r) }
+
+  override def part1: Option[Part] = new Part {
+    override def solution: RIO[Any, Any] = {
+      val result = inputs match {
+        case l :: _ :: ts =>
+          val start = parseAll(rep1(char), l).get
+          val reps  = ts.map(parseAll(replacements, _).get).map { case (l, m, r) => (l, m) -> r }.toMap
+          (1 to 10).foldLeft(start) { case (template, _) =>
+            val x = template.head :: template.sliding(2).toList.flatMap { t => List(reps(t(0) -> t(1)), t(1)) }
+            x
+          }
+        case _ => ???
+      }
+      val sorted = result.groupBy(identity).map { case l -> r => l -> r.size }
+        .toSeq.sortBy(_._2)
+      sorted.last._2 - sorted.head._2
+    }
+      .zio
+  }.some
+
+  override def part2: Option[Part] = new Part {
+    override def solution: RIO[Any, Any] = {
+      val result = inputs match {
+        case l :: _ :: ts =>
+          val start = parseAll(rep1(char), l).get.mkString
+          val reps  = ts.map(parseAll(replacements, _).get).map { case (l, m, r) => s"$l$m" -> r }.toMap
+
+          val counts = start.sliding(2).toSeq.groupBy(identity).map { case l -> r => l -> r.size.big }
+
+          (1 to 40).foldLeft(counts) { case (counts, _) =>
+            counts.toSeq.flatMap { case (p, n) =>
+              Seq(
+                s"${p.toSeq.apply(0)}${reps(p)}" -> n,
+                s"${reps(p)}${p.toSeq.apply(1)}" -> n,
+              )
+            }.groupBy(_._1).view.mapValues(_.map(_._2).sum).toMap
+          }
+        case _ => ???
+      }
+      val sorted =
+        result.toSeq.map { case l -> r => l.head -> r }.groupBy(_._1).view.mapValues(_.map(_._2).sum).toSeq.sortBy(_._2)
+      sorted.last._2 - sorted.head._2 + 1 // Might be off-by-one error here :(
+    }
+      .zio
+  }.some
+
+  def inputs = in2.linesIterator.toList
+
+  lazy val in2 =
+    """NNCB
+      |
+      |CH -> B
+      |HH -> N
+      |CB -> H
+      |NH -> C
+      |HB -> C
+      |HC -> B
+      |HN -> C
+      |NN -> C
+      |BH -> H
+      |NC -> B
+      |NB -> B
+      |BN -> B
+      |BB -> N
+      |BC -> B
+      |CC -> N
+      |CN -> C""".stripMargin
+
+  lazy val in3 =
+    """"""
+
+  override def in: String =
+    """""".stripMargin
+}
