f9c295cf47ce52ba66d28861f5fbc5aa2c11f513
Dave Nicponski <dave.nicponski@gmail.com>
2021-12-20 23:55:40 -0500

    Tweaks to solution base template.

--- 2021/src/main/scala/codekata2021/days/Latest.scala
+++ 2021/src/main/scala/codekata2021/days/Latest.scala
@@ -7,20 +7,24 @@ object Latest extends ParserPuzzle {
   override type PuzzleOut = Any
   override def dayNum: Int = 17
 
-  val word: Parser[String] = "[a-zA-Z]+".r
-  val num: Parser[Int]     = "[0-9]+".r ^^ (_.toInt)
+  private val word: Parser[String]   = "[a-zA-Z]+".r
+  private val num: Parser[Int]       = "[0-9]+".r ^^ (_.toInt)
+  private val neg: Parser[Int]       = "-" ~> num ^^ (-_)
+  private val signedNum: Parser[Int] = num | neg
 
   override def part1: Option[Part] = new Part {
     override def solution: RIO[Any, Any] = {
       ()
+      ()
     }.zio
-  }.some.filter(_ => false)
+  }.some
 
   override def part2: Option[Part] = new Part {
     override def solution: RIO[Any, Any] = {
       ()
+      ()
     }.zio
-  }.some.filter(_ => false)
+  }.some
 
   def inputs = in2
 
