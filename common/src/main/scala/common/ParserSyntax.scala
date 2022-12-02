b0ad09e647e3f643a809de4ebe5db58c0f0786e3
Dave Nicponski <dave.nicponski@gmail.com>
2022-12-02 03:37:15 -0500

    Tweak parsing syntax.
    Note: In future, perhaps make ParserPuzzle inherit from InRegexParserSyntax directly.
    At least, consider once you are sure it will not break older solutions.  The reason
    this would be necessary is that `parseAll` is a method on the `RegexParsers` trait.
    
    Goddamn cake pattern.  Lame.

--- common/src/main/scala/common/ParserSyntax.scala
+++ common/src/main/scala/common/ParserSyntax.scala
@@ -1,10 +1,19 @@
 package common
 
-import scala.util.parsing.combinator.Parsers
+import scala.reflect.ClassTag
+import scala.util.parsing.combinator.{Parsers, RegexParsers}
 
 trait ParserSyntax extends Syntax {
-  implicit class _ParseResultOps[A](in: Parsers#ParseResult[A]) {
+  implicit class _ParserParseResultOps[A](in: Parsers#ParseResult[A]) {
     def getOrFail: A = in.tap(r => if (!r.successful) r.debug: Unit).get
   }
+  implicit class _ParserTildeOps[A, B](in: Parsers#`~`[A, B]) {
+    def toTuple: (A, B) = in._1 -> in._2
+  }
+}
 
+trait InRegexParserSyntax { this: RegexParsers =>
+  implicit class _ParserStringOps(in: String) extends ParserSyntax {
+    def parseLinesBy[A: ClassTag](p: Parser[A]): Seq[A] = in.splitAtLinebreaksBy(parseAll(p, _).getOrFail)
+  }
 }
