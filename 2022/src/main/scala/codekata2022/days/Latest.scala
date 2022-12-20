63b6c124074ee05587a54f3ad394e7df181671f9
Dave Nicponski <dave.nicponski@gmail.com>
2022-12-20 02:32:24 -0500

    WIP: 2022 day 19

--- 2022/src/main/scala/codekata2022/days/Latest.scala
+++ 2022/src/main/scala/codekata2022/days/Latest.scala
@@ -3,7 +3,7 @@ package days
 
 object Latest extends ParserPuzzle {
   override type PuzzleOut = Any
-  override def dayNum: Int = 19
+  override def dayNum: Int = 20
 
   private val word: Parser[String]   = "[a-zA-Z]+".r
   private val num: Parser[Int]       = "[0-9]+".r ^^ (_.toInt)
